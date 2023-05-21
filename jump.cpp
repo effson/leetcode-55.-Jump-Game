a922a7c4166060c30df1e75bfb74e8bd5c9b3d97
effson <113086278+effson@users.noreply.github.com>
2023-05-21 15:41:21 +0800

    jump.cpp

--- jump.cpp
+++ jump.cpp
@@ -1,17 +1,13 @@
 //In this question, you only need to care about the element 0.
-//if nums[i]==0, and for k in range[0,i), nums[i-k]>k,that means you cannot reach the end. 
+//if nums[i]==0, and for k in range(0,i], nums[i-k]<=k,that means you cannot reach the end. 
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         if(nums.empty()||(nums.size()>1&&nums[0]==0))
-        {
             return false;
-        }
         int size=nums.size();
         if(size==1)
-        {
             return true;
-        }
 
         int index=0;
         while(index<size)
@@ -21,25 +17,17 @@ public:
                 index+=1;
             }
             if(index==size-1||index==size)
-            {
                 break;
-            }
             int k=1;
             while(k<=index)
             {
                 if(k<=index&&nums[index-k]>k)
-                {
                     break;
-                }
                 else
-                {
                     k+=1;
-                }
             }
             if(k==index+1)
-            {
                 return false;
-            }
             index+=1;
         }
         return true;  
